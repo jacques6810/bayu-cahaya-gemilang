5e27711b9a9fd7afc4df511c86d73e4985a9a6b1
Jacques Farrell <129415702+jacques6810@users.noreply.github.com>
2025-02-12 21:49:58 +0700

    Update responsive section 3

--- src/Components/Home/Section3.jsx
+++ src/Components/Home/Section3.jsx
@@ -28,68 +28,63 @@ const Section3 = () => {
   ];
 
   return (
-    <div
-      id="section-3"
-      className="flex flex-col md:flex-col lg:flex-row relative z-0 w-full bg-white mt-30 p-4 md:p-10 gap-4"
-    >
-      <div className="flex flex-col items-center mx-auto md:mx-20 mb-4 md:mb-0 w-full lg:w-full p-4">
-        <div
-          id="section-3"
-          className="flex flex-col items-center w-full bg-white p-6 md:p-10"
-        >
-          <img src={Logo} alt="Logo BCG" className="h-12 md:h-16 mb-5" />
-          <h2 className="text-center max-w-xl font-semibold text-xl md:text-4xl leading-snug mb-4">
-            Developer
-          </h2>
+    <div className="flex flex-col items-center mx-auto md:w-full mb-4 md:mb-0 w-full lg:w-full p-4">
+      <div
+        id="section-3"
+        className="flex flex-col items-center w-full bg-white p-6 md:p-10"
+      >
+        <img src={Logo} alt="Logo BCG" className="h-12 md:h-16 mb-5" />
+        <h2 className="text-center max-w-xl font-semibold text-3xl md:text-4xl lg:text-5xl leading-snug mb-4">
+          Developer
+        </h2>
 
-          <div className="relative w-full max-w-5xl">
-            {/* Tombol Panah Kiri */}
-            <button
-              onClick={() => sliderRef.current.slickPrev()}
-              className="mx-4 absolute left-2 top-1/2 transform -translate-y-1/2 z-10 bg-white p-3 rounded-full shadow-lg hover:bg-gray-200 transition"
-            >
-              <FaChevronLeft className="text-teal-500 text-xl" />
-            </button>
+        <div className="relative w-full max-w-5xl">
+          {/* Tombol Panah Kiri */}
+          <button
+            onClick={() => sliderRef.current.slickPrev()}
+            className="mx-4 absolute left-2 top-1/2 transform -translate-y-1/2 z-10 bg-white p-3 rounded-full shadow-lg hover:bg-gray-200 transition"
+          >
+            <FaChevronLeft className="text-teal-500 text-xl" />
+          </button>
 
-            {/* Slider */}
-            <Slider ref={sliderRef} {...settings}>
-              {slides.map((slide, index) => (
-                <div key={index} className="px-3">
-                  <div className="relative h-72 md:h-115 rounded-xl overflow-hidden">
-                    <img
-                      src={slide.image}
-                      alt={slide.title}
-                      className="w-full h-full object-cover"
-                    />
+          {/* Slider */}
+          <Slider ref={sliderRef} {...settings}>
+            {slides.map((slide, index) => (
+              <div key={index} className="px-3">
+                <div className="relative h-72 md:h-115 rounded-xl overflow-hidden">
+                  <img
+                    src={slide.image}
+                    alt={slide.title}
+                    className="w-full h-full object-cover"
+                  />
 
-                    <div className="absolute inset-0 flex items-center justify-center bg-opacity-40">
-                      <div className="absolute w-full h-full bg-black opacity-50 z-0"></div>
-                      <h3 className="mx-18 text-center text-white text-lg md:text-3xl font-bold z-10">
-                        {slide.title}
-                      </h3>
-                    </div>
+                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-40">
+                    <div className="absolute w-full h-full bg-black opacity-50 z-0"></div>
+                    <h3 className="mx-18 text-center text-white text-lg md:text-3xl font-bold z-10">
+                      {slide.title}
+                    </h3>
                   </div>
                 </div>
-              ))}
-            </Slider>
+              </div>
+            ))}
+          </Slider>
 
-            {/* Tombol Panah Kanan */}
-            <button
-              onClick={() => sliderRef.current.slickNext()}
-              className="mx-4 absolute right-2 top-1/2 transform -translate-y-1/2 z-10 bg-white p-3 rounded-full shadow-lg hover:bg-gray-200 transition"
-            >
-              <FaChevronRight className="text-teal-500 text-xl" />
-            </button>
-          </div>
+          {/* Tombol Panah Kanan */}
+          <button
+            onClick={() => sliderRef.current.slickNext()}
+            className="mx-4 absolute right-2 top-1/2 transform -translate-y-1/2 z-10 bg-white p-3 rounded-full shadow-lg hover:bg-gray-200 transition"
+          >
+            <FaChevronRight className="text-teal-500 text-xl" />
+          </button>
+        </div>
 
-          <div className="flex gap-4 mt-6">
-            <button className="text-sm font-semibold px-6 py-3 border-3 border-teal-500 text-teal-500 rounded-lg hover:bg-teal-500 hover:text-white transition min-w-[150px] md:min-w-[200px]">
-              Selengkapnya
-            </button>
-            <button className="text-sm font-semibold px-6 py-3 bg-teal-500 text-white rounded-lg hover:bg-teal-600 transition min-w-[150px] md:min-w-[200px]">
-              Simulasi KPR
-            </button>
-          </div>
+        <div className="flex gap-4 mt-6">
+          <button className="text-sm font-semibold px-6 py-3 border-3 border-teal-500 text-teal-500 rounded-lg hover:bg-teal-500 hover:text-white transition min-w-[150px] md:min-w-[200px]">
+            Selengkapnya
+          </button>
+          <button className="text-sm font-semibold px-6 py-3 bg-teal-500 text-white rounded-lg hover:bg-teal-600 transition min-w-[150px] md:min-w-[200px]">
+            Simulasi KPR
+          </button>
         </div>
       </div>
     </div>
