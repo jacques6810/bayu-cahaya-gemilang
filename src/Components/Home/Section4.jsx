a8af6b109317b86daef5fcca1a0e0f0f53b61d81
Jacques Farrell <129415702+jacques6810@users.noreply.github.com>
2025-02-12 22:34:49 +0700

    Add Section 4

---
+++ src/Components/Home/Section4.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import BackgroundGambar from "../../assets/BackgroundContactUs.jpg";
+import IconPhone from "../../assets/Icon Phone.png";
+
+const Section4 = () => {
+  return (
+    <div
+      id="section-4"
+      className="flex flex-col text-center items-center w-full mx-auto p-6 mt-15 sm:px-20 md:p-10"
+    >
+      <img
+        src={BackgroundGambar}
+        alt=""
+        className="absolute w-full h-100 object-cover"
+      />
+      <div className="absolute w-full h-100 bg-black opacity-50"></div>
+      <div className="flex flex-col items-center my-15 p-2 mx-auto z-0 w-full max-w-5xl">
+        <img src={IconPhone} alt="Phone" className="mb-5 h-15 w-15" />
+        <h1 className="text-white mb-5 text-4xl xl:text-5xl font-semibold z-0">
+          Kontak Kami
+        </h1>
+        <p className="text-white z-0 md:w-1/2 lg:w-1/2">
+          We provide a complete service for the ale, purchase or rental of real
+          estate.
+        </p>
+        <button className="shadow-xl tracking-widest text-sm font-regular px-6 py-2 my-5 bg-teal-600 text-white rounded-lg hover:bg-teal-800 transition min-w-[150px] md:min-w-[200px]">
+          KLIK DISINI
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Section4;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import BackgroundGambar from "../assets/Background Home.png";
 import Section1 from "../Components/Home/Section1";
 import Section2 from "../Components/Home/Section2";
 import Section3 from "../Components/Home/Section3";
+import Section4 from "../Components/Home/Section4";
 import "../App.css";
 
 function Home() {
@@ -13,6 +13,7 @@ function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
