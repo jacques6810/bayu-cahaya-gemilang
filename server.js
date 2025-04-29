94170bdc482637152618059bf5dc8b0895160c4b
Jacques Farrell <129415702+jacques6810@users.noreply.github.com>
2025-04-29 14:55:56 +0700

    Update simulasi bank routes & fetch components

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import aboutInfoRoutes from "./routes/aboutInfoRoutes.js";
 import aboutPointRoutes from "./routes/aboutPointRoutes.js";
 import developerRoutes from "./routes/developerRoutes.js";
 import simulasiRoutes from "./routes/simulasiRoutes.js";
+import simulasiBankRoutes from "./routes/simulasiBankRoutes.js";
 import kontakRoutes from "./routes/kontakRoutes.js";
 
 dotenv.config();
@@ -43,6 +44,7 @@ app.use("/api/about-info", aboutInfoRoutes);
 app.use("/api/about-point", aboutPointRoutes);
 app.use("/api/developer", developerRoutes);
 app.use("/api/simulasi", simulasiRoutes);
+app.use("/api/simulasi-bank", simulasiBankRoutes);
 app.use("/api/kontak", kontakRoutes);
 
 // Jalankan server
