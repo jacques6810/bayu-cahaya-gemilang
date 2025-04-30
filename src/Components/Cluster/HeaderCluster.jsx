dd37b3c526851218ab67a51945d8cbf23732e1a4
Jacques Farrell <129415702+jacques6810@users.noreply.github.com>
2025-04-30 14:09:00 +0700

    Update header cluster, alt background if null

--- src/Components/Cluster/HeaderCluster.jsx
+++ src/Components/Cluster/HeaderCluster.jsx
@@ -1,28 +1,37 @@
 import React from "react";
+import BackgroundAlt from "../../assets/Background Home.png";
 import "../../App.css";
 
 function HeaderCluster({ data }) {
-  // console.log(data.data);
+  const backgroundImage = data.background_header || BackgroundAlt;
+  const hasLogo = !!data.logo_header;
+
   return (
     <div className="relative flex flex-col items-center justify-center mt-20">
       {/* Background */}
       <img
-        src={data.background_header}
+        src={backgroundImage}
         alt="Background"
-        className="w-full h-110 object-cover"
+        className="w-full h-96 sm:h-[28rem] object-cover"
       />
       <div className="absolute w-full h-full bg-black opacity-50"></div>
 
-      {/* Logo Cluster */}
+      {/* Content in center */}
       <div
-        className="absolute flex items-center justify-center inset-0"
+        className="absolute inset-0 flex items-center justify-center"
         data-aos="fade-up"
       >
-        <img
-          src={data.logo_header}
-          alt="Cluster Logo"
-          className="w-32 h-32 bg-white rounded-full p-3 shadow-lg"
-        />
+        {hasLogo ? (
+          <img
+            src={data.logo_header}
+            alt="Cluster Logo"
+            className="w-28 h-28 sm:w-32 sm:h-32 bg-white rounded-full p-3 shadow-lg"
+          />
+        ) : (
+          <h1 className="text-white text-3xl sm:text-5xl font-bold text-center px-4">
+            {data.title || "Judul Cluster"}
+          </h1>
+        )}
       </div>
     </div>
   );
